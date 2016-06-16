fe2acbf90c87aa742d37e495943f04f1406e1076
donepudi4u <donepudi4u@gmail.com>
2016-06-16 11:10:18 -0500

    first RESTFul example

---
+++ RESTFulExample/src/main/java/com/myjavapapers/demo/rest/FirstClass.java
@@ -0,0 +1,20 @@
+package com.myjavapapers.demo.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class FirstClass {
+
+	@RequestMapping(value = "/print")
+	public void printDetails(){
+		System.out.println("You are printing me");
+	}
+	
+	@RequestMapping(value = "/details")
+	public String getDetails(){
+		System.out.println("Testing me");
+		return "You are getting me";
+	}
+}
